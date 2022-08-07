46adcab482606e36000d622dbbd17d32208e0d75
Darrell Payne <darrellpayne@earthlink.net>
2022-08-06 23:00:19 -0700

    Nesting Components

--- src/app.js
+++ src/app.js
@@ -1,3 +1,16 @@
+class IndecisionApp extends React.Component {
+  render() {
+    return(
+      <div>
+        <Header />
+        <Action />
+        <Options />
+        <AddOption />
+      </div>
+    );
+  }
+}
+
 class Header extends React.Component {
   render() {
     return (
@@ -19,9 +32,20 @@ class Action extends React.Component {
   }
 }
 
+class Option extends React.Component {
+  render() {
+    return (
+      <p>Single Option Component here...</p>
+    )
+  }
+}
+
 class Options extends React.Component {
   render() {
-    return <div>Options Components Here</div>;
+    return <div>
+    Options Components Here
+    <Option />
+    </div>;
   }
 }
 
@@ -31,13 +55,12 @@ class AddOption extends React.Component {
   }
 }
 
-const jsx = (
-  <div>
-    <Header />
-    <Action />
-    <Options />
-    <AddOption />
-  </div>
-);
 
-ReactDOM.render(jsx, document.getElementById("app"));
+
+// const jsx = (
+//   <div>
+//     <IndecisionApp />
+//   </div>
+// );
+
+ReactDOM.render(<IndecisionApp />, document.getElementById("app"));
