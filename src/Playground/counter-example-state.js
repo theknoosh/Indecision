7847bfc7db30bddf5ead11ce0505631eafd57724
Darrell Payne <darrellpayne@earthlink.net>
2022-08-13 00:15:49 -0700

    Adding State to Counter App Part II

--- src/Playground/counter-example-state.js
+++ src/Playground/counter-example-state.js
@@ -1,23 +1,38 @@
 class Counter extends React.Component {
-    constructor(props) {
-        super(props);
-        this.handleAddOne.bind(this);
-        this.handleMinusOne.bind(this);
-        this.handleReset.bind(this);
-    }
-    handleAddOne() {
-        console.log("handleAddOne");
-    }
-    handleMinusOne() {
-        console.log("handleMinusOne");
-    }
-    handleReset() {
-        console.log("handleReset");
-    }
+  constructor(props) {
+    super(props);
+    this.handleAddOne = this.handleAddOne.bind(this);
+    this.handleMinusOne = this.handleMinusOne.bind(this);
+    this.handleReset = this.handleReset.bind(this);
+    this.state = {
+      count: 0,
+    };
+  }
+  handleAddOne() {
+    this.setState((prevState) => {
+      return {
+        count: prevState.count + 1,
+      };
+    });
+  }
+  handleMinusOne() {
+    this.setState((prevState) => {
+      return {
+        count: prevState.count - 1,
+      };
+    });
+  }
+  handleReset() {
+    this.setState(() => {
+      return {
+        count: 0,
+      };
+    });
+  }
   render() {
     return (
       <div>
-        <h1>Count: </h1>
+        <h1>Count: {this.state.count}</h1>
         <button onClick={this.handleAddOne}>+1</button>
         <button onClick={this.handleMinusOne}>-1</button>
         <button onClick={this.handleReset}>reset</button>
@@ -26,4 +41,4 @@ class Counter extends React.Component {
   }
 }
 
-ReactDOM.render(<Counter />, document.getElementById('app'));
+ReactDOM.render(<Counter />, document.getElementById("app"));
