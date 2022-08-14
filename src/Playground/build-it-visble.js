77684b1a23ed7035c5acf9528d5acbf825de6e27
Darrell Payne <darrellpayne@earthlink.net>
2022-08-14 07:44:21 -0700

    Build it: Adding State to Visibility Toggle

--- src/Playground/build-it-visble.js
+++ src/Playground/build-it-visble.js
@@ -1,32 +1,71 @@
-const title = "Visability Toggle";
-const copy = "What I want to appear and disappear";
-let isVisible = false;
+class VisibilityToggle extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleToggleVisibility = this.handleToggleVisibility.bind(this);
 
-const makeGone = () => {
-  isVisible = !isVisible;
-  renderApp();
-};
+    this.state = {
+      visibility: false,
+    };
+  }
 
-const appRoot = document.getElementById("app");
+  handleToggleVisibility() {
+    this.setState((prevState) => {
+      return {
+        visibility: !prevState.visibility,
+      };
+    });
+  }
 
-const renderApp = () => {
-  const template = (
-    <div>
-      <h1>{title}</h1>
-      <button onClick={makeGone}>
-        {isVisible ? "Hide Details" : "Show Details"}
-      </button>
-      <p>{isVisible ? copy : undefined}</p>
-      {!isVisible && <p>No details</p>}
-      {isVisible && (
-        <div>
-          <p>{copy}</p>
-        </div>
-      )}
-    </div>
-  );
+  render() {
+    return (
+      <div>
+        <h1>Visibility Toggle</h1>
+        <button onClick={this.handleToggleVisibility}>
+          {this.state.visibility ? "Hide Details" : "Show Details"}
+        </button>
+        <p>
+          {this.state.visibility && (
+            <div>
+              <p>What should appear and disappear</p>
+            </div>
+          )}
+        </p>
+      </div>
+    );
+  }
+}
 
-  ReactDOM.render(template, appRoot);
-};
+ReactDOM.render(<VisibilityToggle />, document.getElementById("app"));
 
-renderApp();
+// const title = "Visability Toggle";
+// const copy = "What I want to appear and disappear";
+// let isVisible = false;
+
+// const makeGone = () => {
+//   isVisible = !isVisible;
+//   renderApp();
+// };
+
+// const appRoot = document.getElementById("app");
+
+// const renderApp = () => {
+//   const template = (
+//     <div>
+// <h1>{title}</h1>
+// <button onClick={makeGone}>
+//   {isVisible ? "Hide Details" : "Show Details"}
+// </button>
+// <p>{isVisible ? copy : undefined}</p>
+// {!isVisible && <p>No details</p>}
+// {isVisible && (
+//   <div>
+//     <p>{copy}</p>
+//         </div>
+//       )}
+//     </div>
+//   );
+
+//   ReactDOM.render(template, appRoot);
+// };
+
+// renderApp();
