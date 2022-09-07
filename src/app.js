2f0a333c956045188b01c853601f1d52add1732d
Darrell Payne <darrellpayne@earthlink.net>
2022-09-06 18:43:46 -0700

    53. Importing NPM modules

--- src/app.js
+++ src/app.js
@@ -1,8 +1,5 @@
-// import './utils.js'
-import subtract, { square, add } from "./utils.js";
+import React from "react";
+import ReactDom from "react-dom";
 
-console.log("app.js is running!!");
-
-console.log(square(4));
-console.log(add(100, 23));
-console.log(subtract(100, 80));
+const template = React.createElement('p', {}, 'testing 123');
+ReactDom.render(template, document.getElementById('app'));
